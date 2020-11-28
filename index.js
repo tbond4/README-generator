3227c9bb116fa564ed2454a899178b1e19b14a56
Tristan Bond <tristanebond@gmail.com>
2020-11-28 12:34:22 -0800

    General readme layout

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-// array of questions for user
-const questions = [
+const inquirer = require('inquirer');
+const fs = require('fs');
+// array of questions for user I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+const questions = ["What is the titel of your project?", "What is your project description?","What are the project installation instructions?",
+"What are the usage instructions?", "What are the contribution guidlines?","What are the test instructions?","What is your GitHub username?","What is your Email?"];
 //names are already given
-];
 
+inquirer.prompt([
+    {
+        type: "input",
+        message: questions[0],
+        name:"title",
+    },
+])
 // function to write README file
 function writeToFile(fileName, data) {
 }
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,41 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+    ${data.description}
+
+  ## Table of COntents
+
+  *[Installation](#installation)
+
+  *[Usage](#usage)
+
+  *[License](#license)
+
+  *[Contributing](#contributing)
+
+  *[Tests](#tests)
+
+  *[Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${data.license}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  ${data.questions}
+  
 `;
 }
 
