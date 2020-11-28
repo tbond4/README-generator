9be3eb58463a4114ee77e105dd7b2c748af7091c
Tristan Bond <tristanebond@gmail.com>
2020-11-28 10:27:26 -0800

    Initail Files

---
+++ index.js
@@ -0,0 +1,16 @@
+// array of questions for user
+const questions = [
+//names are already given
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+//inquierer.prompt.then => pass respobse object to generatemarkdown
+}
+
+// function call to initialize program
+init();
