cf5786e6cfd598b1b1e5fb5342ba84e1240727a3
Tristan Bond <tristanebond@gmail.com>
2020-12-03 19:33:16 -0800

    inputs now write into readme correctly

---
+++ READMENEW.md
@@ -0,0 +1,37 @@
+# adfg
+
+  ## Description
+    kfkf
+
+  ## Table of COntents
+
+  *[Installation](#installation)
+
+  *[Usage](#usage)
+
+  *[License](#license)
+
+  *[Contributing](#contributing)
+
+  *[Tests](#tests)
+
+  *[Questions](#questions)
+
+  ## Installation
+  kfkf
+
+  ## Usage
+  kfkf
+
+  ## License
+  undefined
+
+  ## Contributing
+  fkkf
+
+  ## Tests
+  kfkf
+
+  ## Questions
+  undefined
+  
--- index.js
+++ index.js
@@ -2,47 +2,9 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 // array of questions for user I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-const questions = ["What is the titel of your project?", "What is your project description?","What are the project installation instructions?",
+const questions = ["What is the title of your project?", "What is your project description?","What are the project installation instructions?",
 "What are the usage instructions?", "What are the contribution guidlines?","What are the test instructions?","What is your GitHub username?","What is your Email?"];
 //names are already given
-
-inquirer.prompt([
-    {
-        type: "input",
-        message: questions[0],
-        name:"title",
-    },
-    {
-        type: "input",
-        message: questions[1],
-        name:"title",
-    },
-    {
-        type: "input",
-        message: questions[2],
-        name:"title",
-    },
-    {
-        type: "input",
-        message: questions[3],
-        name:"title",
-    },
-    {
-        type: "input",
-        message: questions[4],
-        name:"title",
-    },
-    {
-        type: "input",
-        message: questions[5],
-        name:"title",
-    },
-    {
-        type: "input",
-        message: questions[6],
-        name:"title",
-    },
-])
 // function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data,(err)=>{ err ? console.log("failed to write file"):console.log("Success!")})
@@ -56,6 +18,36 @@ inquirer.prompt([
         type: "input",
         message: questions[0],
         name:"title",
+    },
+    {
+        type: "input",
+        message: questions[1],
+        name:"description",
+    },
+    {
+        type: "input",
+        message: questions[2],
+        name:"installation",
+    },
+    {
+        type: "input",
+        message: questions[3],
+        name:"usage",
+    },
+    {
+        type: "input",
+        message: questions[4],
+        name:"contributing",
+    },
+    {
+        type: "input",
+        message: questions[5],
+        name:"tests",
+    },
+    {
+        type: "input",
+        message: questions[6],
+        name:"GitHub",
     },
 ]).then(data => {
    writeToFile("READMENEW.md", generateMarkdown(data));
